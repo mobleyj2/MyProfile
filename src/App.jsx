9e6bb8c3940c20afc396616a5eb7e052ac31fb4d
jake <mobleyj2@gmail.com>
2023-08-01 18:31:18 -0400

    fixed errors

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import { Outlet } from 'react-router-dom';
 import Nav from './components/NavTabs';
 import Header from './components/Header';
 import Footer from './components/Footer';
-import './app.css';
+import './App.css';
 
 
 function App() {
