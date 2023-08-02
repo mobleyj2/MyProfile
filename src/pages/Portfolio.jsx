e6f7cc2d15303ffd8872dabfcbf381cff926f1ad
jake <mobleyj2@gmail.com>
2023-08-01 23:09:30 -0400

    added resume

--- src/pages/Portfolio.jsx
+++ src/pages/Portfolio.jsx
@@ -1,76 +1,69 @@
 
+import movie from "./moviemagic.png";
+import data from "./12.png";
+import logo from "./Shapes.png";
+import notes from "./Notes.png";
+import pwa from "./Jate.png";
+import study from "./study.png";
 
 export default function Portfolio() {
   const containerStyle = {
-    display: 'grid',
-    placeItems: 'center',
-    
+    display: "grid",
+    placeItems: "center",
   };
 
-  
-   
-  
-
   const imageStyle = {
-    width: '60%',
-    height: 'auto', // Maintains aspect ratio
-    
+    width: "60%",
+    height: "auto",
   };
 
   return (
     <div>
       <h1>Portfolio</h1>
-      <p>
-        These are just some of the projects I have worked on.
-      </p>
+      <p>These are just some of the projects I have worked on.</p>
       <div style={containerStyle}>
-        <a href ="https://github.com/JuanMartinez503/Movie-Search-Engine.git">Movie</a>
-        <img src="./src/components/moviemagic.png" alt="Movie" style={imageStyle} />
-      </div>
-      <br>
-      </br>
-      
-      <div style={containerStyle}>
-      <a href ="https://github.com/mobleyj2/SQL-Database.git">SQL DataBase</a>
-        <img src="./src/components/12.png" alt="db" style={imageStyle} />
-
+        <a href="https://github.com/JuanMartinez503/Movie-Search-Engine.git">
+          Movie
+        </a>
+        <img src={movie} alt="Movie" style={imageStyle} />
       </div>
 
-      <br>
-      </br>
+      <br />
+
       <div style={containerStyle}>
-      <a href ="https://github.com/mobleyj2/Shapes-and-Name.git">Logo Generator</a>
-        <img src="./src/components/Shapes.png" alt="db" style={imageStyle} />
+        <a href="https://github.com/mobleyj2/SQL-Database.git">SQL DataBase</a>
+        <img src={data} alt="db" style={imageStyle} />
       </div>
 
+      <br />
 
-      <br>
-      </br>
       <div style={containerStyle}>
-      <a href ="https://github.com/mobleyj2/Note-taker.git">Notes </a>
-        <img src="./src/components/notes.png" alt="db" style={imageStyle} />
+        <a href="https://github.com/mobleyj2/Shapes-and-Name.git">Logo Generator</a>
+        <img src={logo} alt="Logo" style={imageStyle} />
       </div>
 
-      <br>
-      </br>
+      <br />
+
       <div style={containerStyle}>
-      <a href ="https://github.com/mobleyj2/PWA-Text-Edit.git">PWA Text</a>
-        <img src="./src/components/Jate.png" alt="db" style={imageStyle} />
+        <a href="https://github.com/mobleyj2/Note-taker.git">Notes</a>
+        <img src={notes} alt="Notes" style={imageStyle} />
       </div>
 
+      <br />
 
-      <br>
-      </br>
       <div style={containerStyle}>
-      <a href ="https://github.com/mobleyj2/study-guide.git">Study Guide</a>
-        <img src="./src/components/study.png" alt="db" style={imageStyle} />
+        <a href="https://github.com/mobleyj2/PWA-Text-Edit.git">PWA Text</a>
+        <img src={pwa} alt="PWA Text" style={imageStyle} />
       </div>
 
-<br>
-</br>
+      <br />
+
+      <div style={containerStyle}>
+        <a href="https://github.com/mobleyj2/study-guide.git">Study Guide</a>
+        <img src={study} alt="Study Guide" style={imageStyle} />
+      </div>
+
+      <br />
     </div>
-
-
-
   );
 }
--- src/pages/Resume.jsx
+++ src/pages/Resume.jsx
@@ -1,25 +1,28 @@
+
+
 export default function Resume() {
-    const containerStyle = {
-      display: 'flex',
-      flexDirection: 'column',
-      alignItems: 'center',
-    };
-  
-    const pdfStyle = {
-      width: '60%',
-      height: '500px', 
-    };
-  
-    return (
-      <div>
-        <h1>Resume</h1>
-        <p></p>
-        <div style={containerStyle}>
-          <a href="/src/components/resume23.pdf">Resume</a>
-          <embed src="./src/components/resume23.pdf" type="application/pdf" style={pdfStyle} />
-        </div>
-        <br/>
+  const containerStyle = {
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+  };
+
+  const pdfStyle = {
+    width: "60%",
+    height: "500px",
+  };
+
+  return (
+    <div>
+      <h1>Resume</h1>
+      <p></p>
+      <div style={containerStyle}>
+        <a href="/components/resume23.pdf" target="_blank" rel="noopener noreferrer">
+          Resume
+        </a>
+        <embed src="./components/resume23.pdf" type="application/pdf" style={pdfStyle} />
       </div>
-    );
-  }
-  
+      <br />
+    </div>
+  );
+}
